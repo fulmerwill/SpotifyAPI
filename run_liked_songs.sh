23fba22650828cb021973f9b11cd6288378b52e2
William Fulmer <92907747+fulmerwill@users.noreply.github.com>
2023-10-11 15:12:54 -0400

    Parameterized table name for added protection against SQL injection.

--- run_liked_songs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment
-source /c/Users/fulme/OneDrive/Desktop/Projects/Spotify\ API/venv/Scripts/activate
-
-# Run your Python script
-python /c/Users/fulme/OneDrive/Desktop/Projects/Spotify\ API/liked_songs.py
